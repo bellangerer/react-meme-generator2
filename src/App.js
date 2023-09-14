b1e8b0b0e6773669afa81b5b6b7c4e2961765d91
bellangerer <bella.angerer@gmail.com>
2023-09-14 16:33:39 +0200

    add data-test-id

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 // used {useState} for adding top and bottom text and also a picture/meme
 import React, { useState } from 'react';
 
@@ -5,50 +7,60 @@ import React, { useState } from 'react';
 function Header() {
   return (
     <header>
-      <h1>Meme Generator</h1>44
+      <div
+        style={{
+          margin: '10px',
+          padding: '9px',
+          border: '6px solid black',
+          borderRadius: '15px',
+        }}
+      >
+        <h1>Meme Generator</h1>
+      </div>
     </header>
   );
 }
 // here is the function {useState}
 function MemeGenerator() {
   // added const (variable that can't be changed)
-  const [topText, setTopText] = useState('');
+  const [topText, setTopText] = useState('hello');
   const [bottomText, setBottomText] = useState('');
   const [memeImageUrl, setMemeImageUrl] = useState('https://memegen.link/');
 
   // this is the const function to generate the text, picture etc.
   const generateMeme = () => {};
-
+  console.log(topText);
+  console.log(bottomText);
   return (
     <div className="meme-generator">
-      <h1>Meme Generator</h1>
-
-      <label htmlFor="topText">Top text</label>
-
-      <input
-        type="text"
-        id="topText"
-        value={topText}
-        onChange={(e) => setTopText(e.target.value)}
+      <img
+        src={`https://api.memegen.link/images/doge/${topText}/${bottomText}.png?height=300&width=400`}
+        alt="Meme"
+        data-test-id="meme-image" // data-test-id="generate-meme"
       />
 
-      <label htmlFor="bottomText">Bottom Text</label>
+      <label>
+        {' '}
+        <input value={topText} onChange={(e) => setTopText(e.target.value)} />
+      </label>
 
       <input
-        type="text"
-        id="bottomText"
         value={bottomText}
         onChange={(e) => setBottomText(e.target.value)}
       />
 
-      <label htmlFor="memeTemplate">Meme template</label>
-
-      <input
-        type="text"
-        id="memeTemplate"
-        value={memeTemplate}
-        onChange={(e) => setMemeTemplate(e.target.value)}
-      />
+      <button onClick={generateMeme}>Generate</button>
     </div>
   );
 }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
+}
+
+export default App;
